d1128884773bd3988d7f6a8c80a374c79f209e1e
shentongle <shentongle@h3d.com.cn>
2017-05-23 19:10:39 +0800

    add IRay

---
+++ source/Irrlicht/IRay.h
@@ -0,0 +1,17 @@
+#ifndef __I_RAY_H_INCLUDED__
+#define __I_RAY_H_INCLUDED__
+
+#include "IReferenceCounted.h"
+
+namespace irr {
+namespace ray {
+
+	class IRay : public IReferenceCounted
+	{
+
+	};
+
+}
+}
+
+#endif // __I_RAY_H_INCLUDED__
